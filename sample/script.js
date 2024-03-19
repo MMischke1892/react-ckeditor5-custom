b329c6fe37b336db90bfb100e9d0b840ff6adf48
ivansazdovski <ivan.sazdovski@hotmail.com>
2024-03-19 17:16:48 +0100

    Initial commit

---
+++ sample/script.js
@@ -0,0 +1,20 @@
+ClassicEditor
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "wpoc53ucf2aa-xe2t9aq7a1mf" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
